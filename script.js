6811ac50777a522317be4160acb3bae19727ca66
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-11-19 14:48:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor (species){
+		this._species =species
+	}
 
-class Dog extends Animal {}
+	get species(){
+		return this._species;
+	}
+	makeSound(){
+		console.log("The Golden Retriever makes a sound")
+	}
+}
 
-class Cat extends Animal {}
+class Dog extends Animal {
+
+	bark(){
+		console.log("woof")
+	}
+}
+
+class Cat extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  purr() {
+    console.log("purr");
+  }
+
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
